916818be5f97bd7ddc745559ea39f08aed29f0e3
Aenstein <leonid.od@bk.ru>
2025-12-19 02:41:13 +0300

    Create methods: Create, Delete, SendMessage

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,62 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
 
+	"github.com/brianvoe/gofakeit"
 	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	desc "github.com/Aenstein/chat-server/pkg/note_v1"
 )
 
+const (
+	grpcPort = 50051
+)
+
+type server struct {
+	desc.UnimplementedNoteV1Server
+}
+
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("Chat group: %v", req.GetNames())
+
+	return &desc.CreateResponse{
+		Id: gofakeit.Int64(),
+	}, nil
+}
+
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("Delete id %d", req.GetId())
+
+	return &emptypb.Empty{}, nil
+}
+
+func (s *server) SendMessage(ctx context.Context, req *desc.SendMessageRequest) (*emptypb.Empty, error) {
+	log.Printf("%s send: \"%s\"", req.GetFrom(), req.GetText())
+
+	return &emptypb.Empty{}, nil
+}
+
 func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterNoteV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 	fmt.Println(color.GreenString("Hello, World!"))
 }
--- go.mod
+++ go.mod
@@ -1,11 +1,21 @@
 module github.com/Aenstein/chat-server
 
-go 1.23.2
+go 1.24.0
 
-require github.com/fatih/color v1.18.0
+toolchain go1.24.11
+
+require (
+	github.com/brianvoe/gofakeit v3.18.0+incompatible
+	github.com/fatih/color v1.18.0
+	google.golang.org/grpc v1.77.0
+	google.golang.org/protobuf v1.36.11
+)
 
 require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/net v0.46.1-0.20251013234738-63d1a5100f82 // indirect
+	golang.org/x/sys v0.37.0 // indirect
+	golang.org/x/text v0.30.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251022142026-3a174f9686a8 // indirect
 )
